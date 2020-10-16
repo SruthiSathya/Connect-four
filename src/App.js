b41b28e3f26523806370eba84a3a6083d14b6fc1
Fire Fly <44548964+SruthiSathya@users.noreply.github.com>
2020-10-17 03:52:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import themes from './utils/themes';
+
+const App = () => {
+  const [theme, setTheme] = useState('');
+
+  const handleChange = (selectedTheme) => {
+    setTheme(themes[selectedTheme.value]);
+  };
+
+  const refCallback = (node) => {
+    if (node) {
+      theme &&
+        Object.keys(theme).forEach((element) => {
+          node.style.setProperty(element, theme[element], 'important');
+          if (element === 'background-color' || element === 'background') {
+            // apply the same background mentioned for theme to the body of the website
+            document.body.style.background = theme[element];
+          }
+        });
+    }
+  };
+
+  return (
+    
+    <div ref={refCallback} className="main">
+      
+     <Header handleChange={handleChange} />
+      <Main/>
+    </div>
+
+    
+  );
+};
+
+export default App;
---
+++ src/utils/themes.js
@@ -0,0 +1,38 @@
+const themes = {
+  default: {
+    'background-color': '#dbdbdb',
+    'color': '#000',
+    'font-family': "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+  },
+  Normal: {
+    'background-color': '#fff',
+    'color': '#847d7d',
+    'font-family': 'Verdana, Geneva, Tahoma, sans-serif'
+  },
+  Dark: {
+    'background-color': '#000',
+    'color': '#fff',
+  },
+  Yellow: {
+    'background-color': '#ebca12',
+    'color': '#a50b0b',
+    'font-family': "'Courier New', Courier, monospace"
+  },
+  Green: {
+    'background-color': '#4a7548',
+    'color': '#847d7d',
+    'font-family': 'Verdana, Geneva, Tahoma, sans-serif'
+  },
+  Red: {
+    'background-color': '#4a7548',
+    'color': '#847d7d',
+    'font-family': 'Verdana, Geneva, Tahoma, sans-serif'
+  },
+  Pink: {
+  'background-color': '#fd9be5',
+  'color': '#847d7d',
+  'font-family': 'Verdana, Geneva, Tahoma, sans-serif'
+}
+}; 
+
+export default themes;
